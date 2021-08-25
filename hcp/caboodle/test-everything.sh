3ca1b1ec60188b58ecd8041f71dc60ba6535e1af
Geoff Thorpe <geoffrey@twosigma.com>
2021-08-25 15:07:57 -0400

    hcp: decorate caboodle's test-everything.sh
    Use "sleep 1" as the can't-possibly-go-wrong way to ensure a backgrounded
    service is ready. Also, add some comments to explain that this really is
    for kicking around and development, not anything resembling production.
    
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/caboodle/test-everything.sh
+++ hcp/caboodle/test-everything.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# NOTES
+#  - the processes started by this test do not get stopped, so attempting to
+#    run it a second time will fail.
+#    - however, exiting the container will stop them.
+#  - the one-time setup in this test is backed onto persistent storage, so
+#    restarting the container will not automatically return you to a pristine
+#    state.
+#    - however "make hcp_clearall" will clear out the persistent storage.
+
 # HCP Enrollment Service.
 if [[ ! -d $HCP_ENROLLSVC_STATE_PREFIX/enrolldb.git ]]; then
 	/hcp/enrollsvc/setup_enrolldb.sh
@@ -9,23 +18,27 @@ fi
 /hcp/enrollsvc/run_mgmt.sh &
 /hcp/enrollsvc/run_repl.sh &
 
+# We _could_ tail_wait.pt the enrollsvc msgbus outputs to make sure they're
+# truly listening before we launch things (like attestsvc) that depend on it.
+# But ... nah. Let's just sleep for a second instead.
+sleep 1
+
 if [[ ! -d $HCP_ATTESTSVC_STATE_PREFIX/A ]]; then
 	/hcp/attestsvc/setup_repl.sh
 fi
 /hcp/attestsvc/run_repl.sh &
 /hcp/attestsvc/run_hcp.sh &
 
-if [[ ! -d $HCP_SWTPMSVC_STATE_PREFIX/ek.pub ]]; then
+# Same comment;
+sleep 1
+
+if [[ ! -f $HCP_SWTPMSVC_STATE_PREFIX/ek.pub ]]; then
 	/hcp/swtpmsvc/setup_swtpm.sh
 fi
 /hcp/swtpmsvc/run_swtpm.sh &
 
+# Same comment;
+sleep 1
+
 /hcp/client/run_client.sh
 
-# TODO: the following doesn't work yet because of process control issues. (The wrapper
-# scripts get terminated, but some child processes keep running.)
-#echo die > /msgbus/enrollsvc-mgmt-ctrl
-#echo die > /msgbus/enrollsvc-repl-ctrl
-#echo die > /msgbus/attestsvc-repl-ctrl
-#echo die > /msgbus/attestsvc-hcp-ctrl
-#echo die > /msgbus/swtpmsvc-ctrl
