531bc9ddb56f72dfb5676065e3fb214c3390e034
Geoff Thorpe <geoffrey@twosigma.com>
2021-09-01 13:28:12 -0400

    hcp: improve enroll.py and document a curl/jq equivalent
    This script is part of swtpmsvc, because we use it to enroll a new sTPM's
    EKpub as it is generated.
    
    Importantly though, it also serves as reference code for any users/tooling
    that need to hit the enrollment API programmatically. This is implemented
    in python and is explanatory, but we also add (in comments at the top) a
    quick-and-dirty way of performing the same operation by calling 'curl'
    directly and (optionally) extracting the result code from the response
    using 'jq'.
    
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/swtpmsvc/enroll.py
+++ hcp/swtpmsvc/enroll.py
@@ -1,12 +1,26 @@
-# This simple python script can be used to automate the enrollment of a host
+#!/usr/bin/python3
+
+# A crude way to do this task with a single command-line (using curl and jq) is;
+#
+# curl -v -F ekpub=@$TPM_EKPUB \
+#         -F hostname=$HCP_SWTPMSVC_ENROLL_HOSTNAME \
+#         $HCP_SWTPMSVC_ENROLL_URL | jq -r '.returncode'
+#
+# This will be '0' on success, '1' on failure.
+
+
+# The following python script can be used to automate the enrollment of a host
 # with the HCP Enrollment Service. The latter exposes an API end-point
 # (typically at <URL-base>/v1/add) for us to hit, and takes 2 parameters that
 # are both specified via environment variables;
+#
 # - hostname: specified by the "HCP_SWTPMSVC_ENROLL_HOSTNAME" environment
 #   variable.
+#
 # - ekpub: the public part of the TPM's "Endorsement Key", either in its raw
 #   TPM binary format or PEM-encoded. The "TPM_EKPUB" environment variable
 #   provides the path to this file.
+#
 # The URL for this API must also be provided by the "HCP_SWTPMSVC_ENROLL_URL"
 # environment variable.
 #
@@ -23,12 +37,20 @@ import requests
 import os
 import sys
 
+ekpub = os.environ.get('TPM_EKPUB')
+hostname = os.environ.get('HCP_SWTPMSVC_ENROLL_HOSTNAME')
+api_url = os.environ.get('HCP_SWTPMSVC_ENROLL_URL')
+
+assert ekpub, "'TPM_EKPUB' must be set to the path of ek.pub"
+assert hostname, "'HCP_SWTPMSVC_ENROLL_HOSTNAME' must be the hostname enrolled with this TPM key"
+assert api_url, "'HCP_SWTPMSVC_ENROLL_URL' must be set to the enrollment API URL"
+
 form_data = {
-    'ekpub': ('ek.pub', open(os.environ.get('TPM_EKPUB'), 'rb')),
-    'hostname': (None, os.environ.get('HCP_SWTPMSVC_ENROLL_HOSTNAME'))
+    'ekpub': ('ek.pub', open(ekpub, 'rb')),
+    'hostname': (None, hostname)
 }
 
-response = requests.post(os.environ.get('HCP_SWTPMSVC_ENROLL_URL'), files=form_data)
+response = requests.post(api_url, files=form_data)
 
 jr = json.loads(response.content)
 
