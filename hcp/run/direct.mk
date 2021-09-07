80add8fc630a458736d5a6cfc0656dfbdddeb7ea
Geoff Thorpe <geoffrey@twosigma.com>
2021-09-07 16:08:11 -0400

    hcp/run: reorganize and improve run-time controls.
    Rather than duplicating settings between the "direct" workflow (in
    hcp/run/direct.mk) and the source dev/debug workflow (in hcp/Makefile), we
    organize for the latter to use the former. This involves it setting
    HCP_RUN_SETTINGS of course, but it also involves separating the controls
    between those that identify images and paths (direct.mk will defer if they
    are already set, which they will be in the source workflow) and those that
    provide application configuration (direct.mk sets these unconditionally,
    as you would edit it or bypass it if you intend anything else).
    
    With this change, the full set of enrollsvc and attestsvc controls are now
    available to the user of the direct workflow.
    
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/run/direct.mk
+++ hcp/run/direct.mk
@@ -16,14 +16,24 @@
 #     $ ln -s ./hcp/run/Makefile GNUmakefile
 #     $ make [...]
 #
-# The settings below are intended to match those used (by default) in the
-# safeboot source environment. I.e. once HCP images are built using the source
-# workflow and defaults, we can operate the services using either the source
-# workflow _or_ the hcp/run workflow directly. The settings in this file should
-# result in the same/interchangable usage, and that is how we can maintain this
+# Though the settings below allow hcp/run/Makefile to be used directly, without
+# dependence on the safeboot source environment, they also get used when using
+# the source environment is present. I.e. the build system automatically sets
+# HCP_RUN_SETTINGS to point to this file, provides HCP_RUN_*** values for the
+# "images and paths" properties, and then includes hcp/run/Makefile and relies
+# on this file being automatically included to provide the HCP_RUN_*** values
+# for "application properties".
+#
+# So from within the dev/debug workflow we can operate the services _or_ we can
+# pivot to the hcp/run workflow to operate them directly. The application
+# settings in this file get used in both cases so should result in the
+# same/interchangable usage, and that is how we can maintain this
 # operation/production-focussed workflow while developing in the dev/debug
 # workflow.
 
+####################
+# Images and paths #
+####################
 
 # In the dev/debug workflow, "make" is always run from the top-level directory
 # of the source tree, puts all build-related and other generated artifacts
@@ -49,10 +59,32 @@ HCP_RUN_ASSIST_CLEANUP ?= $(TOP)/hcp/assist_cleanup.sh
 # container-based cleanup) is this;
 HCP_RUN_UTIL_IMAGE ?= debian:bullseye-slim
 
-# Application knobs
+##########################
+# Application properties #
+##########################
+
+# Commented-out settings show defaults.
+
+#HCP_RUN_ENROLL_UWSGI := uwsgi_python3
+#HCP_RUN_ENROLL_UWSGI_PORT := 5000
+#HCP_RUN_ENROLL_UWSGI_FLAGS := --http :5000 --stats :5001
+#HCP_RUN_ENROLL_UWSGI_OPTIONS := --processes 2 --threads 2
+#HCP_RUN_ENROLL_GITDAEMON := /usr/lib/git-core/git-daemon
+#HCP_RUN_ENROLL_GITDAEMON_FLAGS := --reuseaddr --verbose --listen=0.0.0.0 --port=9418
+HCP_RUN_ENROLL_XTRA_MGMT := -p 5000:5000 -p 5001:5001
+HCP_RUN_ENROLL_XTRA_REPL := -p 9418:9418
+
 HCP_RUN_ATTEST_REMOTE_REPO := git://enrollsvc_repl/enrolldb
 HCP_RUN_ATTEST_UPDATE_TIMER := 10
+#HCP_RUN_ATTEST_UWSGI := uwsgi_python3
+#HCP_RUN_ATTEST_UWSGI_PORT := 8080
+#HCP_RUN_ATTEST_UWSGI_FLAGS := --http :8080 --stats :8081
+#HCP_RUN_ATTEST_UWSGI_OPTIONS := --processes 2 --threads 2
+#HCP_RUN_ATTEST_XTRA_REPL :=
+HCP_RUN_ATTEST_XTRA_HCP := -p 8080:8080 -p 8081:8081
+
 HCP_RUN_SWTPM_ENROLL_HOSTNAME := example_host.wherever.xyz
 HCP_RUN_SWTPM_ENROLL_URL := http://enrollsvc_mgmt:5000/v1/add
+
 HCP_RUN_CLIENT_TPM2TOOLS_TCTI := swtpm:host=swtpmsvc,port=9876
 HCP_RUN_CLIENT_ATTEST_URL := http://attestsvc_hcp:8080
