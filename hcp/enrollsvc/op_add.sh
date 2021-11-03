5436ac77b83556acc35403e1091ac1a29bc77391
Geoff Thorpe <geoffrey@twosigma.com>
2021-11-02 22:49:54 -0400

    hcp: Make op_add.sh return JSON and log to stderr only
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/enrollsvc/op_add.sh
+++ hcp/enrollsvc/op_add.sh
@@ -4,13 +4,13 @@
 
 expect_db_user
 
-echo "Starting $0"
-echo "  - Param1=$1 (path to ek.pub/ek.pem)"
-echo "  - Param2=$2 (hostname)"
+echo "Starting $0" >&2
+echo "  - Param1=$1 (path to ek.pub/ek.pem)" >&2
+echo "  - Param2=$2 (hostname)" >&2
 
 # args must be non-empty
 if [[ -z $1 || -z $2 ]]; then
-	echo "Error, missing at least one argument"
+	echo "Error, missing at least one argument" >&2
 	exit 1
 fi
 
@@ -36,14 +36,11 @@ export EPHEMERAL_ENROLL=`mktemp -d -u`
 
 ./sbin/attest-enroll -C /safeboot/enroll.conf \
 		-V CHECKOUT=/hcp/enrollsvc/cb_checkout.sh \
-		-V COMMIT=/hcp/enrollsvc/cb_commit.sh -I $1 $2 ||
-	(echo "Error, 'attest-enroll' failed" && exit 1) || exit 1
-
-# When running from a console, it can be handy to see what was generated
-ls -l $EPHEMERAL_ENROLL
+		-V COMMIT=/hcp/enrollsvc/cb_commit.sh -I $1 $2 >&2 ||
+	(echo "Error, 'attest-enroll' failed" >&2 && exit 1) || exit 1
 
 [[ -f "$EPHEMERAL_ENROLL/ek.pub" ]] ||
-	(echo "Error, ek.pub file not where it is expected" && exit 1) || exit 1
+	(echo "Error, ek.pub file not where it is expected" >&2 && exit 1) || exit 1
 
 EKPUBHASH="$(sha256sum "$EPHEMERAL_ENROLL/ek.pub" | cut -f1 -d' ')"
 HALFHASH=`echo $EKPUBHASH | cut -c 1-16`
@@ -54,14 +51,14 @@ cd $REPO_PATH
 # Also, make sure nothing (sane) causes an exit/abort without us making it to
 # the unlock. Any error should set 'itfailed', and no subsequent steps should
 # run unless 'itfailed' isn't set.
-repo_cmd_lock || (echo "Error, failed to lock repo" && exit 1) || exit 1
+repo_cmd_lock || (echo "Error, failed to lock repo" >&2 && exit 1) || exit 1
 
 # Ensure an "exclusive" enrollment, i.e. if the directory already exists, the
 # TPM is already enrolled, and we're not (yet) supporting enrollment
 # modifications!
 ply_path_add "$EKPUBHASH" || itfailed=1
 [[ -z "$itfailed" ]] && [[ ! -d $FPATH ]] ||
-	(echo "Error, TPM is already enrolled" && exit 1) || itfailed=1
+	(echo "Error, TPM is already enrolled" >&2 && exit 1) || itfailed=1
 [[ -z "$itfailed" ]] && mkdir -p $FPATH || itfailed=1
 
 # Combine the existing hn2ek with the new entry, sort the result, and put in
@@ -75,7 +72,7 @@ ply_path_add "$EKPUBHASH" || itfailed=1
 		cp -a $EPHEMERAL_ENROLL/* "$FPATH/" &&
 		mv $HN2EK_PATH.tmp $HN2EK_PATH &&
 		git add . &&
-		git commit -m "map $HALFHASH to $2") || itfailed=1
+		git commit -m "map $HALFHASH to $2") >&2 || itfailed=1
 # TODO:
 # 1. This exception/error/rollback path (necessarily before releasing the lock)
 #    needs an alert valve of some kind. It's implemented to maximise
@@ -91,7 +88,7 @@ ply_path_add "$EKPUBHASH" || itfailed=1
 [[ -z "$itfailed" ]] ||
 	(echo "Failure, attempting recovery" &&
 		echo "running 'git reset --hard'" && git reset --hard &&
-		echo "running 'git clean -f -d -x'" && git clean -f -d -x) ||
+		echo "running 'git clean -f -d -x'" && git clean -f -d -x) >&2 ||
 	rollbackfailed=1
 
 # If recovery failed, refuse to unlock the repo, forcing an intervention and
@@ -103,5 +100,7 @@ ply_path_add "$EKPUBHASH" || itfailed=1
 
 rm -rf $EPHEMERAL_ENROLL
 
-echo "installed at \"$FPATH\""
+jq -Rn --arg hostname "$2" --arg ekpubhash "$HALFHASH" \
+	'{returncode: 0, hostname: $hostname, ekpubhash: $ekpubhash}'
+
 /bin/true
