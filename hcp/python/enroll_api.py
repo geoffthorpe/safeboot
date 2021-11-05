37d353080a09e2e78a36e9e9b40dc2088af67a51
Geoff Thorpe <geoffrey@twosigma.com>
2021-11-05 17:22:42 -0400

    hcp: prefer 'requests-gssapi' over 'requests-kerberos'
    Signed-off-by: Geoff Thorpe <geoffrey@twosigma.com>

--- hcp/python/enroll_api.py
+++ hcp/python/enroll_api.py
@@ -23,11 +23,19 @@ import os
 import sys
 import argparse
 
-if os.environ.get('HCP_KERBEROS'):
-    from requests_kerberos import HTTPKerberosAuth, DISABLED
-    auth = HTTPKerberosAuth(mutual_authentication=DISABLED, force_preemptive=True)
-else:
-    auth = None
+auth = None
+if os.environ.get('HCP_GSSAPI'):
+    print('Enabling GSSAPI authentication')
+    try:
+        from requests_gssapi import HTTPSPNEGOAuth, DISABLED
+        auth = HTTPSPNEGOAuth(mutual_authentication=DISABLED, opportunistic_auth=True)
+    except ModuleNotFoundError:
+        print("'requests-gssapi' unavailable, falling back to 'requests-kerberos'")
+        try:
+            from requests_kerberos import HTTPKerberosAuth, DISABLED
+            auth = HTTPKerberosAuth(mutual_authentication=DISABLED, force_preemptive=True)
+        except ModuleNotFoundError:
+            print("'requests-kerberos' unavailable, falling back to no authentication")
 
 # TODO: make this interface more import-friendly and less cmd-line-specific.
 # I.e. the enroll_*() functions tag an 'args' structure for all parameters,
